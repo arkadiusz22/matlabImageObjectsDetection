59065ffb46915ce66a482e64cf0c026f21ec7d1e
arkadiusz22 <arek.zawadzki.22@gmail.com>
2020-01-05 22:49:51 +0100

    Added rectangle and triangle detection + forrmating

--- script.m
+++ script.m
@@ -1,19 +1,14 @@
-clc;    % Clear the command window.
-close all;  % Close all figures (except those of imtool.)
-imtool close all;  % Close all imtool figures.
-clear;  % Erase all existing variables.
-
-%% Prepare figure - fullscreen
-figure;
-% fig=gcf;
-% fig.Units='normalized';
-% fig.OuterPosition=[0 0 1 1];
+clc; % Clear the command window.
+close all; % Close all figures (except those of imtool.)
+imtool close all; % Close all imtool figures.
+clear; % Erase all existing variables.
 
 %% Read image
+figure;
 originalImage = imread('img/4.png');
 subplot(3, 3, 1);
 imshow(originalImage);
-title('Original color Image'); 
+title('Original color Image');
 
 %% Convert to grey
 grayImage = rgb2gray(originalImage);
@@ -28,74 +23,81 @@ imshow(binarizedImage);
 title('Converted to binary image');
 
 %% Canny edge detection
-cannyImage = edge(binarizedImage,'canny');
+cannyImage = edge(binarizedImage, 'canny');
 subplot(3, 3, 4);
 imshow(cannyImage);
 title('Canny Edge Image');
 
 %% Sobel edge detection
-sobelImage = edge(binarizedImage,'sobel');
+sobelImage = edge(binarizedImage, 'sobel');
 subplot(3, 3, 5);
 imshow(sobelImage);
 title('Sobel Edge Image');
 
 %% Prewitt edge detection
-prewittImage = edge(binarizedImage,'prewitt');
+prewittImage = edge(binarizedImage, 'prewitt');
 subplot(3, 3, 6);
 imshow(prewittImage);
 title('Prewitt Edge Image');
 
 %% Added edges
-allEdgesMethods = cannyImage+sobelImage+prewittImage;
+allEdgesMethods = cannyImage + sobelImage + prewittImage;
 subplot(3, 3, 7);
 imshow(allEdgesMethods);
 title('Added all detected edges');
 
 %% Fill detected holes (figures)
-filledHoles = imfill(allEdgesMethods,'holes');
+filledHoles = imfill(allEdgesMethods, 'holes');
 subplot(3, 3, 8);
 imshow(filledHoles)
 title('Filled Image')
 
-%% Figures properties detection 
-[B,L] = bwboundaries(filledHoles, 'noholes');
+%% Figures properties detection
+[B, L] = bwboundaries(filledHoles, 'noholes');
 STATS = regionprops(L, 'all'); % we need 'BoundingBox' and 'Extent'
+numberOfShapes = length(STATS);
+shapes = zeros(size(numberOfShapes));
 
 %% Prepare results figure
 figure;
 imshow(originalImage),
 title('Results');
-hold on
+hold on;
 
-%% Analyze each figure properties and display according label
-for i = 1 : length(STATS)
-  if (uint8(abs(STATS(i).BoundingBox(3)-STATS(i).BoundingBox(4)) < 0.1))
-% squerish 
-   if (abs(STATS(i).Extent - 1) < 0.05)
-% square
-         W(i) = 1;
-   elseif (abs(STATS(i).Extent - 1) < 0.30)
-% circle
-         W(i) = 2;
-   end
-  elseif (uint8(abs(STATS(i).BoundingBox(3)-STATS(i).BoundingBox(4)) < 0.3))
-     W(i) = 0;
-  else 
-     W(i) = 0;
+%% Analyze each figure properties
+for i = 1 : numberOfShapes
+  if (abs(STATS(i).BoundingBox(3) - STATS(i).BoundingBox(4)) < 0.1) && (abs(STATS(i).Extent) > 0.95)
+    shapes(i) = 1; % square
+  elseif (abs(STATS(i).Extent) > 0.95)
+    shapes(i) = 2; % rectangle
+  elseif (abs(STATS(i).BoundingBox(3) - STATS(i).BoundingBox(4)) < 0.1) && (abs(STATS(i).Extent) > 0.70)
+    shapes(i) = 3; % circle
+  elseif (abs(STATS(i).Extent) > 0.25) && (abs(STATS(i).Extent) < 0.6)
+    shapes(i) = 4; % triangle
+  else
+    shapes(i) = 0; % other
   end
-  
+end
+
+%% Display name of each shape
+for i = 1 : numberOfShapes
+  txtOffset = 25;
+  switch shapes(i)
+    case 1
+      txt = 'Square';
+    case 2
+      txt = 'Rectangle';
+      txtOffset = 35;
+    case 3
+      txt = 'Circle';
+    case 4
+      txt = 'Triangle';
+      txtOffset = 30;
+    otherwise
+      txt = 'Other';
+  end
   centroid = STATS(i).Centroid;
- 
-  switch W(i)
-     case 1
-        txt = 'Square';
-     case 2
-        txt = 'Circle';
-     otherwise
-        txt = 'Other';
-  end 
-  t = text(centroid(1)-25,centroid(2),txt);
+  t = text(centroid(1) - txtOffset, centroid(2), txt);
   t.Color = 'white';
   t.FontSize = 10;
 end
-
